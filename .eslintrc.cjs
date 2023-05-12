351f0a75e1d0321a53b3a8d0070ebd76a8f31a83
Magomed Mamergov <mamergov@mateline.ru>
2023-05-12 14:33:02 +0300

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    '@typescript-eslint/semi': 0,
+    'react/function-component-definition': 0,
+    'arrow-body-style': 0,
+  },
+}
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+const App = () => {
+  const asd = 'asd'
+
+  var str = 'string'
+
+  console.log('asds')
+
+  return (
+    <form>
+      <input
+        id="dinosaur_name"
+        name="dinosaur_name"
+        className="dinosaur_name"
+        type="text"
+        aria-describedby="dinosaur_name_info"
+      />
+    </form>
+  )
+}
+
+export default App
